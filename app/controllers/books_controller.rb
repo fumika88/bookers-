a149610bc32d80ca758f7254b6fdf60427463f0b
EC2 Default User <ec2-user@ip-172-31-7-248.ec2.internal>
2021-03-09 12:16:58 +0000

    bokkers作成途中

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+  def new
+    # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
+    @book = Book.new
+  end
+  def create
+    # １. データを新規登録するためのインスタンス作成
+    @book = Book.new(book_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    @book.save
+    # ３. トップ画面へリダイレクト
+    redirect_to book_path(@book.id)
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+   @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+   @book = Book.find(params[:id])  # データ（レコード）を1件取得
+    @book.destroy  # データ（レコード）を削除
+    redirect_to books_path  # 投稿一覧画面へリダイレクト
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+   params.require(:book).permit(:title, :body)
+  end
+end
+
